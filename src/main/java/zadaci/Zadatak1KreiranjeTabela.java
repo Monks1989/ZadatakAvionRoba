b020f800fa22969035bfc019597413e972eed966
Vlado <jovesvladislav@gmail.com>
2019-09-30 20:07:58 +0200

    Kreirane klase za zadatke

--- src/main/java/zadaci/Zadatak1KreiranjeTabela.java
+++ src/main/java/zadaci/Zadatak1KreiranjeTabela.java
@@ -1,4 +1,18 @@
 package zadaci;
 
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+//import orm.lite.model.*;
+
 public class Zadatak1KreiranjeTabela {
+    public static void main(String[] args) {
+        ConnectionSource connectionSource = null;
+       try {
+           connectionSource  = new JdbcConnectionSource("jdbc:sqlite:avionRoba.db");
+           connectionSource.close();
+       }catch (Exception e) {
+           e.printStackTrace();
+        }
+    }
 }
--- src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
+++ src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
@@ -1,4 +1,16 @@
 package zadaci;
 
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+
 public class Zadatak3IzmenaVrednosti {
+    public static void main(String[] args) {
+        ConnectionSource connectionSource = null;
+        try {
+            connectionSource  = new JdbcConnectionSource("jdbc:sqlite:avionRoba.db");
+            connectionSource.close();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
